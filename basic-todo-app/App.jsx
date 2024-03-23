5d3b238635d1fbb65ee427faaeb03fd33ef1ed44
rajesh-kumar-work <work.kumar.rajesh@gmail.com>
2024-03-23 10:21:59 +0530

    creating list item

--- basic-todo-app/App.jsx
+++ basic-todo-app/App.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import ListItem from "./components/ListItem";
 
 export default function App() {
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         bgcolor: "skyblue",
         height: "100vh",
+        width: "100vw",
         padding: "0px",
         margin: "0px",
+        paddingLeft: "25%",
+        paddingTop: "5%",
       }}
     >
       <Box
         sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           bgcolor: "white",
-          p: "0px",
+          padding: "32px",
           m: "0px",
           width: "50%",
           borderRadius: "16px",
         }}
       >
-        <h1>Hello</h1>
+        <h1>ToDo List</h1>
+
+        <ListItem></ListItem>
       </Box>
     </Box>
   );
--- basic-todo-app/components/ListItem.jsx
+++ basic-todo-app/components/ListItem.jsx
@@ -1,19 +1,43 @@
 import Paper from "@mui/material/Paper";
-import React from "react";
 import Box from "@mui/material/Box";
+import React from "react";
+import Checkbox from "@mui/material/Checkbox";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function ListItem() {
   return (
-    <Paper
+    <Box
       sx={{
-        padding: "8px",
-        bgcolor: "crimson",
-        color: "white",
-        minWidth: "50%",
+        width: "100%",
       }}
-      elevation={2}
     >
-      <h1>Hello</h1>
-    </Paper>
+      <Paper
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "2px 0px 2px 16px",
+          bgcolor: "#DCDCDC",
+          color: "black",
+          borderRadius: "8px",
+          fontSize: "18px",
+        }}
+        elevation={2}
+      >
+        <p>This is the first to do rtasj</p>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "2px 0px 2px 16px",
+            bgcolor: "#DCDCDC",
+            color: "black",
+          }}
+        >
+          <DeleteIcon />
+          <Checkbox />
+        </Box>
+      </Paper>
+    </Box>
   );
 }
