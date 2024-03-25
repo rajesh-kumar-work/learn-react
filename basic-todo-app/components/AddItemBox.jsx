53537cd59fa0fe3d7daaa8ab91f48b469fc5d4d2
rajesh-kumar-work <work.kumar.rajesh@gmail.com>
2024-03-25 11:43:37 +0530

    error handling

--- basic-todo-app/components/AddItemBox.jsx
+++ basic-todo-app/components/AddItemBox.jsx
@@ -8,53 +8,75 @@ AddItemBox.propTypes = {
 
 export default function AddItemBox({ handleAdd }) {
   const [newTask, setNewTask] = useState("");
+  const [error, setError] = useState(false);
 
   const onAdd = () => {
+    if (newTask.trim() === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
     setNewTask("");
     handleAdd(newTask);
   };
+
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: "space-between",
+        flexDirection: "column",
         padding: "0px",
         marginBottom: "32px",
-        color: "black",
-        borderRadius: "32px",
-        bgcolor: "#ebebeb",
         width: "80%",
       }}
     >
-      <input
-        value={newTask}
-        onChange={(event) => setNewTask(event.target.value)}
-        style={{
-          flex: "1",
-          backgroundColor: "#ebebeb",
-          border: "none",
-          borderRadius: "32px",
-          fontSize: "20px",
-          paddingLeft: "5px",
-          color: "green",
-          textDecoration: "none",
-        }}
-      ></input>
-      <Button
-        onClick={onAdd}
+      <Box
         sx={{
-          bgcolor: "#32CD32",
-          borderRadius: "50% ",
-          fontSize: "50px",
+          display: "flex",
+          flex: "1",
+          justifyContent: "space-between",
+          padding: "0px",
           color: "black",
-          height: "60px",
-          width: "60px",
-          fontFamily: "monospace",
+          borderRadius: "32px",
+          bgcolor: "#ebebeb",
         }}
-        variant="contained"
       >
-        +
-      </Button>
+        <input
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+          style={{
+            flex: "1",
+            backgroundColor: "#ebebeb",
+            border: "none",
+            borderRadius: "32px",
+            fontSize: "20px",
+            paddingLeft: "5px",
+            color: "green",
+            textDecoration: "none",
+            outline: "none",
+          }}
+        ></input>
+        <Button
+          onClick={onAdd}
+          sx={{
+            bgcolor: "#32CD32",
+            borderRadius: "50% ",
+            fontSize: "50px",
+            color: "black",
+            height: "60px",
+            width: "60px",
+            fontFamily: "monospace",
+          }}
+          variant="contained"
+        >
+          +
+        </Button>
+      </Box>
+      {error && (
+        <p style={{ color: "red", paddingLeft: "10PX" }}>
+          New Task Cannot Be Empty
+        </p>
+      )}
     </Box>
   );
 }
